b594d5bd01c44ac22b0ede46516f5dda9ce4bbbf
praktikant101 <ek27global312@gmail.com>
2023-04-24 05:29:38 +0600

    Step 6.1: Refactor (app)

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
-from django.core.cache import cache
 
 from rest_framework import generics
 from rest_framework.views import APIView
